10efabd2bcba37f1086cced6c9623014de5b37f0
Kayleigh Soane <kayleigh.soane@students.plymouth.ac.uk>
2025-10-17 12:46:59 +0100

    Completed

--- Task4 - Arrays Strings and Pointers/Task4/Task4.cpp
+++ Task4 - Arrays Strings and Pointers/Task4/Task4.cpp
@@ -2,10 +2,72 @@
 //
 
 #include <iostream>
+#include <cstring>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+	void task1();
+		int Farray[10];
+		for (int i = 1; i <= 10; i++) {
+			Farray[i-1] = i;
+			cout << Farray[i-1] << " ";
+			
+		}
+		cout << endl;
+		cout << endl;
+
+
+	void task2();
+		string a = "My name is ";
+		string b = "Kayleigh.";
+		string c = a + b;
+		cout << c << endl;
+		cout << "The length of the two strings is " << c.length() << endl;
+		if (a.length() == b.length()) {
+			cout << "The strings are equal in length" << endl;
+		}
+		else {
+			cout << "The strings are not equal in length" << endl;
+		}
+		cout << endl;
+
+
+	void task3();
+		int value = 5;
+		int* xPtr = &value;									// int* xPtr declares a pointer to an integer and initializes it with the address of value
+		cout << "The value is: " << *xPtr << endl;			// Writing xPtr prints the address of value, but writing *xPtr prints the value stored at that address
+		int newvalue = *xPtr + 10;							// Printing *xPtr is the same as printing value
+		cout << "The new value is: " << newvalue << endl;
+		cout << endl;
+
+
+	void task4();
+		int arrayP[5] = { 4,67,43,2,7 };
+		for (int i = 0; i < 5; i++) {
+			int* yPtr = &arrayP[i];
+			cout << "Element " << i << " is " << *yPtr << endl;
+		}
+		cout << endl;
+		for (int i = 0; i < 5; i++) {
+			int* yPtr = &arrayP[i];
+			cout << "Element " << i << " plus 2 is " << *yPtr + 2 << endl;
+		}
+		cout << endl;
+
+
+	void task5();
+		char* Poo = new char[20];						// 'new' idynamically allocates
+		cout << "Input 19 characters" << endl;			// Last character of array is null terminator
+		cin.getline(Poo, 20);							// Reads input (up to 19 characters the null terminator) and stores it in Poo					
+		cout << Poo << endl;
+		cout << "Length: " << strlen(Poo) << endl;
+		delete[] Poo;									// Frees the memory allocated for the character array Poo. 'delete' only works for memory allocated with 'new'
+		
+
+
+		
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
